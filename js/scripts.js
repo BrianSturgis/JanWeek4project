2003bb6ec158700af24fac6a22409635f9f58059
Brian Sturgis <sturujisu@gmail.com>
2021-01-31 10:54:23 -0800

    this update project before overhaul

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,5 @@ function Order(name,address,phoneNumber) {
   this.currentId = 0;
 }
 
-Order.prototype.addPizzaId = function(pizza, address) {
-  pizza.id = this.assignId();
-  address.id = pizza.id;
-  this.pizzas[pizza.id] = pizza;
-  this.address[address.id] = address;
-}
 
-Order.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-}
-
-let pizza = 10;
-let address = 10;
-
-
-let testObj = new Order(pizza,address);
-testObj.addPizzaId();
-console.log(testObj);
-console.log("hi");
 
