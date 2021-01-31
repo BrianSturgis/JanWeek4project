b5d39eed52783662553439ded297b5c789bd5721
Brian Sturgis <sturujisu@gmail.com>
2021-01-30 21:28:15 -0800

    a negative fail for the assignId function

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
 // business logic
 function Order(name,address,phoneNumber) {
-  this.name = name;
-  this.address = address;
-  this.phoneNumber = phoneNumber;
+  this.pizzas = {};
+  this.address = {};
+  this.currentId = 0;
 }
 
+Order.prototype.addPizzaId = function(pizza, address) {
+  pizza.id = this.assignId();
+  address.id = pizza.id;
+  this.pizzas[pizza.id] = pizza;
+  this.address[address.id] = address;
+}
+
+Order.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+let pizza = 10;
+let address = 10;
 
 
-
-let testObj = new Order();
+let testObj = new Order(pizza,address);
+testObj.addPizzaId();
 console.log(testObj);
 console.log("hi");
 
-//ui logic
