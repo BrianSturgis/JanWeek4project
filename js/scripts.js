714c2e9849660336c7ff1ebfabed7eff7b220c3e
Brian Sturgis <sturujisu@gmail.com>
2021-01-31 18:21:37 -0800

    edits to home.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,26 @@
 function Order() {
   this.pizzas = {};
   this.address = [];
-  
 }
 
 Order.prototype.addPizza = function(pizza) {
   this.pizzas[pizza.size] = pizza;
 }
 
-// business logic for pizz
+Order.prototype.addAddress = function(address) {
+  this.address[address.size] = address;
+}
+
 function Pizza (size,toppings,drink) {
   this.size = size;
   this.toppings = toppings;
   this.drink = drink;
+  this.totalPrice = 0;
+}
+
+Pizza.prototype.totalCost = function(){
+  let price = this.size + this.toppings + this.drink;
+  this.totalPrice = price;
 }
 
 function Address (name,address,phoneNumber) {
@@ -22,17 +30,30 @@ function Address (name,address,phoneNumber) {
   this.phoneNumber = phoneNumber;
 }
 
-
-
-
-
-let newOrder = new Order();
-let newPizza01 = new Pizza(12,14,12);
-let newPizza02 = new Pizza(13,14,13);
-let address = new Address();
-newOrder.addPizza(newPizza01);
-newOrder.addPizza(newPizza02);
-console.log(address);
-console.log(newOrder);
-console.log(newPizza01);
-console.log(newPizza02);
+// ui logic
+$(document).ready(function(event) {
+  
+  let inputtedSize =$();
+  let inputtedToppings =$();
+  let inputtedDrink =$();
+  let inputtedTotalPrice =$();
+  let inputtedName =$();
+  let inputtedAddress =$();
+  let inputtedPhoneNumber =$();
+  console.log("hi");
+  $();
+  $();
+  
+// let newOrder = new Order();
+// let address = new Address("brian", "15010 nw towle terrace", 666-4001);
+// let newPizza01 = new Pizza(12,14,12);
+// let newPizza02 = new Pizza(13,14,13);
+// newPizza01.totalCost();
+// newPizza02.totalCost();
+// newOrder.addPizza(newPizza01);
+// newOrder.addPizza(newPizza02);
+// newOrder.addAddress(address);
+// console.log(address);
+// console.log(newOrder);
+// console.log(newPizza01);
+// console.log(newPizza02);
